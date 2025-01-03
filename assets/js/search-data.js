b52cbe81ed28045e004bfb016db79f2182d61e8e
mbraihan <12432136+mbraihan@users.noreply.github.com>
2025-01-03 03:17:34 +0000

    Deploying to gh-pages from @ mbraihan/mbraihan.github.io@e1ee9e37bd1ae6f01ac79d736e24cc7e6491e789 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -492,6 +492,13 @@ ninja.data = [{
           window.open("https://www.semanticscholar.org/author/2303687304", "_blank");
         },
       },{
+        id: 'social-x',
+        title: 'X',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://twitter.com/rbfahim", "_blank");
+        },
+      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
