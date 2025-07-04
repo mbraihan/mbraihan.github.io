2f431ca208abed6c7c9fbb55cd4745331f70e00f
mbraihan <12432136+mbraihan@users.noreply.github.com>
2025-07-04 00:08:42 +0000

    Deploying to gh-pages from @ mbraihan/mbraihan.github.io@4f66f0bc6e26e5dd5f9e573f1d4c4174383d9878 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,22 +3,22 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-home",
+    title: "Home",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-publications",
-          title: "publications",
-          description: "If you&#39;re searching for papers on a specific topic, try exploring the bibliographies linked to each research project or use the search box, which supports keywords like &quot;Selection.&quot;",
+          title: "Publications",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
           },
         },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "CV",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
