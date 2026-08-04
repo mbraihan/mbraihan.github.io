483bb3de34007399fbb0b4d084b11b5611e6f811
mbraihan <rbfahim.iub13@outlook.com>
2026-08-04 13:25:50 -0400

    Update logo's

--- src/data/institutions.ts
+++ src/data/institutions.ts
@@ -9,7 +9,7 @@ const institutions: Record<string, Institution> = {
   'Dalhousie University': { name: 'Dalhousie University', logo: 'https://www.dal.ca/favicon.ico' },
   'Simon Fraser University': { name: 'Simon Fraser University', logo: 'https://www.sfu.ca/favicon.ico' },
   'Kadir Has University': { name: 'Kadir Has University', logo: 'https://www.khas.edu.tr/favicon.ico' },
-  'Aarhus University': { name: 'Aarhus University', logo: '/assets/institutions/aarhus-university.webp' },
+  'Aarhus University': { name: 'Aarhus University', logo: '/assets/institutions/aarhus-university.png' },
   'University of Bath': { name: 'University of Bath', logo: 'https://www.bath.ac.uk/favicon.ico' },
   'Google Inc.': { name: 'Google', logo: 'https://www.google.com/favicon.ico' },
 };
